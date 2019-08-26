74c8a9e47bdd74a719d7f6066c8e3557c83874dc
Joshua Koch <joshua.b.koch@gmail.com>
2019-08-26 16:05:30 -0500

    Add segmentize executable

---
+++ apps/segmentize.cpp
@@ -0,0 +1,48 @@
+
+// --- Lwgeom Includes --- //
+extern "C"
+{
+#include <liblwgeom.h>
+}
+
+// --- Standard Includes --- //
+#include <cfloat>
+#include <iostream>
+#include <string>
+
+////////////////////////////////////////////////////////////////////////////////
+int main(
+    int argc,
+    char** argv )
+{
+    bool success = false;
+    int has_bbox = 0;
+    int precision = DBL_DIG;
+    char* srs = nullptr;
+    LWGEOM* gin = nullptr;
+    LWGEOM* gout = nullptr;
+
+    try
+    {
+        if( argc != 3 ) throw std::invalid_argument(
+            "Wrong number of arguments provided" );
+        std::string geojson = argv[ 1 ];
+        double dist = std::stod( argv[ 2 ] );
+        gin = lwgeom_from_geojson( geojson.c_str(), &srs );
+        gout = lwgeom_segmentize2d( gin, dist );
+        geojson = lwgeom_to_geojson( gout, srs, precision, has_bbox );
+        std::cout << geojson;
+        success = true;
+    }
+    catch( std::exception const& e )
+    {
+        std::cerr << "*** std::exception caught: " << e.what() << std::endl;
+    }
+
+    lwfree( srs );
+    lwfree( gin );
+    lwfree( gout );
+
+    return ( success ) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+////////////////////////////////////////////////////////////////////////////////
